a3c2e8d2b3f026f3f5ad64e21bc49a82c6f518c1
ro-dahal <rohan.dahal929@gmail.com>
2025-07-16 17:09:00 +0545

    b

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// loading animations
+
+
+window.addEventListener("load", function () {
+    const preloader = document.getElementById("preloader");
+
+    // Add delay (e.g., 1.5 seconds) before starting fade out
+    setTimeout(() => {
+      preloader.style.opacity = '0'; // Start fade-out
+      preloader.style.transition = 'opacity 0.8s ease'; // Smooth transition
+
+      // Optional: fully remove from DOM after fade-out completes
+      setTimeout(() => {
+        preloader.style.display = 'none';
+      }, 200); // Match this to your transition duration
+    }, 500); // Delay before fade-out starts
+  });
+
+
 document.getElementById('trackOrderStartBtn').addEventListener('click', function() {
   document.getElementById('trackOrderDetails').classList.remove('hidden-track-order');
 });
+
+
+
+
+
+
+
